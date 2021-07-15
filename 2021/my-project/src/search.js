c4d6f8b603f39c06a17537f03ff0b24329ec6f37
zhangtao <zhangt12@mingyuanyun.com>
2021-07-15 21:31:54 +0800

    feat: 增加less解析、webpack5的热更新

--- 2021/my-project/src/search.js
+++ 2021/my-project/src/search.js
@@ -1,1 +1,16 @@
-document.write("search");
+import React from "react";
+import ReactDom from "react-dom";
+
+import "./index.less";
+import ReduxImg from "./img/redux.jpg";
+import WebpackLogo from "./img/webpack.png";
+const App = () => {
+  return (
+    <>
+      <div className="search">我是serach --watch + devServer  </div>
+      <img src={ReduxImg} alt="图片" />
+      <img src={WebpackLogo} alt="图片" />
+    </>
+  );
+};
+ReactDom.render(<App />, document.getElementById("root"));
