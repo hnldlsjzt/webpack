fd5427669645774312dceae40d31ee07d70081ae
hnldlsjzt <819163569@qq.com>
2021-07-17 16:53:59 +0800

    feat: 查看tree shaking的作用

--- 2021/my-project/src/search/index.js
+++ 2021/my-project/src/search/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { add } from "../common/unit";
+import { a } from "./tree-shaking";
 import "./index.less";
 import ReduxImg from "./img/redux.jpg";
 import WebpackLogo from "./img/webpack.png";
@@ -10,7 +11,7 @@ const App = () => {
       <div className="search">我是serach --watch + devServer </div>
       <div>.hot-updata.json</div>
       <div>整体刷新 if hot</div>
-      <div>2+3={add(2, 3)}</div>
+      <div>2+3={(add(2, 3), a())}</div>
       <img src={ReduxImg} alt="图片" />
       <img src={WebpackLogo} alt="图片" />
     </>
---
+++ 2021/my-project/src/search/tree-shaking.js
@@ -0,0 +1,9 @@
+// 查看 tree-shaking 是否起作用
+
+export function a() {
+  return "this is func a";
+}
+
+export function b() {
+  return "this is func b";
+}
