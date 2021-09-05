9bf07a445fe71860ad65209531639975f76381f8
hnldlsjzt <819163569@qq.com>
2021-09-05 18:40:44 +0800

    feat: 增加同步、异步的回调使用，增加抛异常的两种方式

--- 2021/row-loader/src/row-loader.js
+++ 2021/row-loader/src/row-loader.js
@@ -1,8 +1,25 @@
 const loaderUtils = require("loader-utils");
+const fs = require("fs");
+const path = require("path");
 // 接收一个参数
 module.exports = function (resource) {
   const { name } = loaderUtils.getOptions(this);
-  console.log('name', name);// 获取到test
+  console.log("name", name); // 获取到test
   let string = JSON.stringify(resource).replace("foo", "zee");
-  return `export default ${string}`;
+
+  // 返回单个与多个参数
+  // return `export default ${string}`;
+  // this.callback(null, string, 1, 2, 3, 4); //result: [ '"zeebar"', 1, 2, 3, 4 ],
+  // 抛异常的两种方式
+  // throw new Error('Error')
+  // this.callback(new Error("callback Error"), string, 1, 2, 3, 4); // callback Error
+  // ---- 以上都是同步的方式，以下增加异步的方式------------
+  const callback = this.async(); // 异步 loader 的返回方式
+  fs.readFile(path.join(__dirname, "./async.txt"), function (err, data) {
+    if (err) {
+      callback(err, "");
+      return;
+    }
+    callback(null, data.toString());
+  });
 };
